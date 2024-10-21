eb0a0bc993854cc587b0d77f844e8d1bd4be2ae1
GUNiclasEricsson <gusniclaer@student.gu.se>
2024-10-21 14:26:06 +0200

    started on the workout model

--- trainingapp/lib/components/workout.dart
+++ trainingapp/lib/components/workout.dart
@@ -1,3 +1,25 @@
 // add workout class
-// needs workout title, current weather, weight, time, date, exercise, reps, sets 
+// needs workout title, current weather, weight, time, date, exercise, reps, sets
 //
+
+class Workout {
+  String? workoutName = '';
+  String? currentWeather = '';
+  int? weight;
+  int? time;
+  int? date;
+  String exercise = '';
+  int reps = 1;
+  int sets = 1;
+
+  Workout({
+    required this.workoutName,
+    required this.currentWeather,
+    required this.weight,
+    required this.time,
+    required this.date,
+    required this.exercise,
+    required this.reps,
+    required this.sets,
+  });
+}
