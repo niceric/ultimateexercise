f82d98cdbffb879c5655b5d2b34aa5474dafe5f0
GUNiclasEricsson <gusniclaer@student.gu.se>
2024-10-21 14:58:02 +0200

    started adding routing for add_exercise

--- trainingapp/lib/screens/create_workout.dart
+++ trainingapp/lib/screens/create_workout.dart
@@ -1,13 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:trainingapp/states/screen_index_provider.dart';
+import 'package:provider/provider.dart';
 
 class CreateWorkout extends StatelessWidget {
   const CreateWorkout({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final _screenindexprovider = Provider.of<ScreenIndexProvider>(context);
+    int currentScreenIndex = _screenindexprovider.fetchCurrentScreenIndex;
     return Scaffold(
       appBar: AppBar(
         title: Text('Create Workout'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              IconButton(
+                  onPressed: () {
+                    currentScreenIndex == 5;
+                  },
+                  icon: Icon(Icons.plus_one))
+            ],
+          )
+        ],
       ),
     );
   }
